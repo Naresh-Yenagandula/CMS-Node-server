9bc28612fe168416b6e15bc4552695196446e9a5
unknown <nareshyenagandula98@gmail.com>
2021-04-14 15:44:46 +0530

    Done get data from server

--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -19,13 +19,6 @@ router.post('/pages',async (req,res)=>{
     }
 });
 
-router.get('/user',async (req,res)=>{
-    const data = await User.find();
-    if(!data) return res.status(400).json({message:"No Data"});
-
-    if(data) return res.status(200).json(data);
-});
-
 router.post('/categories',async (req,res)=>{
     const catg = new categories({
         title:req.body.title,
@@ -37,6 +30,27 @@ router.post('/categories',async (req,res)=>{
     } catch (error) {
         res.status(400).json({message:"Failed to add category"})
     }
+});
+
+router.get('/user',async (req,res)=>{
+    const data = await User.find();
+    if(!data) return res.status(400).json({message:"No Data"});
+
+    if(data) return res.status(200).json(data);
+});
+
+router.get('/pages',async (req,res)=>{
+    const data = await Page.find();
+    if(!data) return res.status(400).json({message:"No Data"});
+
+    if(data) return res.status(200).json(data);
+});
+
+router.get('/categories',async (req,res)=>{
+    const data = await categories.find();
+    if(!data) return res.status(400).json({message:"No Data"});
+
+    if(data) return res.status(200).json(data);
 });
 
 
